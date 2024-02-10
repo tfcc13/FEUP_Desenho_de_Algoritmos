be84c462a811a8f616d26e9e8ff350549213ddf6
Tiago <tiagofccarvalho13@gmail.com>
2024-02-10 15:54:19 +0000

    made exercise 1 a

---
+++ P01/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/tiago/Desktop/FEUP/2Ano/2Semestre/Desenho_de_Algoritmos/P01/lib/googletest/googlemock
+# Build directory: C:/Users/tiago/Desktop/FEUP/2Ano/2Semestre/Desenho_de_Algoritmos/P01/cmake-build-debug/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ P01/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/tiago/Desktop/FEUP/2Ano/2Semestre/Desenho_de_Algoritmos/P01/lib/googletest/googletest
+# Build directory: C:/Users/tiago/Desktop/FEUP/2Ano/2Semestre/Desenho_de_Algoritmos/P01/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
