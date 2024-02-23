563d25b6e131a37c232a080b3bf203e9c08f7d96
tfcc13 <tiagofccarvalho13@gmail.com>
2024-02-23 08:49:06 +0000

    P2 first commit

---
+++ P02/da2324_p02_student/TP2/ex1.cpp
@@ -0,0 +1,19 @@
+// By: Gonçalo Leão
+// With contributions by Eduardo Almeida
+
+int maxSubsequence(int A[], unsigned int n, unsigned int &i, unsigned int &j) {
+    // TODO
+    return 0;
+}
+
+/// TESTS ///
+#include <gtest/gtest.h>
+
+TEST(TP2_Ex1, maxSubsequence) {
+    int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    unsigned int n = 9;
+    unsigned int i, j;
+    EXPECT_EQ(maxSubsequence(A,n,i,j), 6);
+    EXPECT_EQ(i, 3);
+    EXPECT_EQ(j, 6);
+}
